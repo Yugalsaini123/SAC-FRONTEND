35abb9b32a0903aa7bf0b58c5c8593f7965ff4a1
Yugal Saini <yugalsaini2002@gmail.com>
2024-10-07 09:47:37 +0530

    Fixed hero image for signup page

--- src/components/MainContent.jsx
+++ src/components/MainContent.jsx
@@ -18,10 +18,10 @@ const MainContent = () => {
       <div className="relative w-full md:w-3/5 h-[calc(100vh-80px)] overflow-hidden">
         <img src={bgImage} alt="Background" className="w-[calc(100vh-80px)] h-[calc(100vh-80px)] object-cover object-left-top" />
       </div>
-      <div className="text-left p-4 md:p-0">
+      <div className="w-full md:w-1/2 text-left justify-center items-center   ">
         <h1 className="text-4xl font-bold text-gray-800">Welcome to ClubCon</h1>
         <p className="text-lg text-gray-600 mb-4">Join the community that's right for you.</p>
-        <div className="flex gap-4">
+        <div className="flex gap-4 p-4 md:p-0">
           <button onClick={navigateToSignup} className="px-6 py-3 bg-gray-300 rounded hover:bg-gray-400">Create an Organisation</button>
           <button onClick={navigateToLogin} className="px-6 py-3 bg-gray-300 rounded hover:bg-gray-400">Login</button>
         </div>
--- src/components/SignupPage.jsx
+++ src/components/SignupPage.jsx
@@ -12,9 +12,9 @@
    };
  
    return (
-     <div className="flex flex-col md:flex-row h-screen bg-gray-100">
-       <div className="w-full md:w-1/2 bg-blue-100 flex justify-center items-center overflow-hidden">
-         <img src={bgImage} alt="Background" className="w-4/5 max-w-md h-auto" />
+     <div className="flex flex-col md:flex-row items-center justify-between p-0 ">
+       <div className="relative w-full md:w-3/5 h-[calc(100vh-80px)] overflow-hidden">
+         <img src={bgImage} alt="Background" className="w-[calc(100vh-80px)] h-[calc(100vh-80px)] object-cover object-left-top  " />
        </div>
        <div className="w-full md:w-1/2 flex justify-center items-center bg-white">
          <div className="w-4/5 max-w-md bg-white p-6 shadow-lg rounded-lg text-center">
@@ -29,7 +29,7 @@
              By creating an account you agree with our 
              <a href="#" className="text-blue-600 hover:underline"> Terms of Service</a>, <a href="#" className="text-blue-600 hover:underline">Privacy Policy</a>, 
              and our default <a href="#" className="text-blue-600 hover:underline">Notifications Settings</a>.
-             Already have an account? <a href="#" className="text-blue-600 hover:underline">Sign In</a>
+             Already have an account? <a href="/login" className="text-blue-600 hover:underline">Sign In</a>
            </p>
          </div>
        </div>
